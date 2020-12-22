3b99fdb367fa2653aacc110ea4d2fc029f6374b5
Meziane Dahou <hd_meziane@esi.dz>
2020-12-22 18:00:43 +0100

    Adding timer progress

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,29 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     @IBOutlet weak var txtLabel: UILabel!
-    let eggTimes = ["Soft": 60 ,"Medium": 4 , "Hard": 7]
-    var secondsRemaining = 60
+    let eggTimes = ["Soft": 3 ,"Medium": 4 , "Hard": 7]
     var timer = Timer()
+    var totalTime = 0
+    var secondPassed = 0
     var hardness = ""
     @IBOutlet weak var progressBar: UIProgressView!
     @IBAction func eggBtnPressed(_ sender: UIButton) {
         timer.invalidate()
         txtLabel.text = "How do you like your eggs ?"
         hardness = sender.currentTitle!
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
         
         
     }
     @objc func updateTimer(){
-        progressBar.progress = Float((eggTimes[hardness]! - secondsRemaining)/eggTimes[hardness]!)
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondPassed < totalTime {
+            let percentageProgress = Float(secondPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+            secondPassed += 1
+            
         }else {
-            txtLabel.text = "Done !"
+            progressBar.progress = 1.0
+            secondPassed = 0
+            timer.invalidate()
+            txtLabel.text = "Done"
         }
         
     }
